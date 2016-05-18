11668e04268d36618a3e07f13725a1d2bb059e43
Shloka Desai <shlokadesai@email.arizona.edu>
2016-05-17 20:34:27 -0700

    fixed untracked files

--- components/user-list/user-listController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-cs142App.controller('UserListController', ['$scope',
-    function ($scope) {
-        $scope.main.title = 'Users';
-
-        console.log('window.cs142models.userListModel()', window.cs142models.userListModel());
-    }]);
-
