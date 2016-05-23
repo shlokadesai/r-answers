d3588df256958b8a50b6273d10671e225528a2d0
Shloka Desai <shlokadesai@email.arizona.edu>
2016-05-23 13:12:59 -0700

    Completed sign up

--- components/signUp4/signUp4Controller.js
+++ components/signUp4/signUp4Controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-refugeeAnswers.controller('SignUpPage4Controller', ['$scope', '$routeParams',
+refugeeAnswers.controller('SignUp4Controller', ['$scope', '$routeParams',
   function ($scope, $routeParams) {
 
   }]);
